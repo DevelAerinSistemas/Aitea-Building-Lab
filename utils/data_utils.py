c82442d8443513cbc24325cf1c87737d6b47c5cc
Jose Luis Blanco <joseluis.blanco@aitea.tech>
2024-10-06 12:39:47 +0200

    utils

---
+++ utils/data_utils.py
@@ -0,0 +1,82 @@
+'''
+ # @ Author: Jose Luis Blanco Garcia-Moreno <joseluis.blanco@aitea.tech>
+ # @ Create Time: 2024-09-04 09:38:30
+ # @ Modified by: Jose LUis Blanco Garcia-Moreno
+ # @ Modified time: 2024-09-04 09:38:33
+ # @ Proyect: Aitea Building Lab 
+ # @ Description:
+ # @ Copyright (c) 2024: Departamento de I+D. Aitea Tech
+ '''
+
+from loguru import logger
+import pandas as pd
+import time
+
+def synchronization_and_optimization(stream_data: pd.DataFrame) -> pd.DataFrame:
+    """Synchronize and reduce the size of the dataframe received from influx
+
+    Args:
+        stream_data (pd.DataFrame (stream)): Stream dataframe (generator)  
+
+    Returns:
+        pd.DataFrame: Total dataframe (None if is empty)
+    """
+    dataframe_list = []
+    for one_data in stream_data:
+        one_data["_value"] = one_data["_value"].astype('float32') 
+        df_selected = one_data.reset_index(
+        )[['_time', 'system_id', 'floor', '_field', '_value']]
+        df_pivot = df_selected.pivot_table(
+        # Las columnas que mantendremos como índice
+        index=['_time', 'system_id', 'floor'],
+        # La columna cuyos valores se convertirán en nuevas columnas
+        columns='_field',
+        values='_value',                        # Los valores de las nuevas columnas
+        # Función para manejar duplicados (puedes ajustar si es necesario)
+        ).reset_index()
+        dataframe_list.append(df_pivot)
+    if len(dataframe_list) > 0:
+        data_pivot_all = pd.concat(dataframe_list, ignore_index=True)
+        data_pivot_all["_time"] = data_pivot_all['_time'].dt.ceil('min')
+        data_pivot_all["_time"] = data_pivot_all['_time'].dt.round('5min')
+        data_pivot_all = data_pivot_all.groupby(
+            ['_time', 'system_id', 'floor']).mean().reset_index()
+        total_nan =  data_pivot_all.isna().sum().sum()
+        logger.info(f"Total number of NaN's after synchronization: {total_nan} in {data_pivot_all.shape}")
+        data_pivot_all = fill_nan(data_pivot_all)
+        total_nan =  data_pivot_all.isna().sum().sum()
+        logger.info(f"Total number of NaN's after synchronization: {total_nan} in {data_pivot_all.shape}")
+        data_pivot_all = data_pivot_all.dropna()
+        data_pivot_all["_time"] = data_pivot_all['_time'].astype('int64') // 10**9 
+        for one_column in data_pivot_all.columns:
+            if not one_column in ["_time",]:
+                if one_column in ["floor", "system_id"]:
+                    data_pivot_all[one_column] = data_pivot_all[one_column].astype('category')
+                elif one_column not in ["room_temperature", "setpoint_temperature"]:
+                    data_pivot_all[one_column] = data_pivot_all[one_column].astype('int8')
+                else:
+                    data_pivot_all[one_column] = data_pivot_all[one_column].astype('float16')
+        mem_usage_per_column = data_pivot_all.memory_usage(deep=True)
+        mem_usage_total = mem_usage_per_column.sum()
+        logger.info(f"Total dataframe size: {mem_usage_total / (1024 ** 2):.2f} MB")
+        if data_pivot_all.empty:
+            logger.warning("Empty dataframe")
+            data_pivot_all = None
+    else:
+        data_pivot_all = None
+        logger.warning("Empty dataframe")
+    return data_pivot_all
+
+
+def fill_nan(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe_list = []
+    system_ids = dataframe["system_id"].unique()
+    for system_id in system_ids:
+        one = dataframe[dataframe["system_id"] == system_id]
+        one  = one.ffill(limit=2)
+        one  = one.bfill(limit=2)
+        dataframe_list.append(one)
+    if len(dataframe_list) > 0:
+        dataframe = pd.concat(dataframe_list)
+    return dataframe
+        
---
+++ utils/pipe_utils.py
@@ -0,0 +1,88 @@
+'''
+ # @ Author: Jose Luis Blanco Garcia-Moreno <joseluis.blanco@aitea.tech>
+ # @ Create Time: 2024-09-26 12:25:32
+ # @ Modified by: Jose LUis Blanco Garcia-Moreno
+ # @ Modified time: 2024-09-26 12:26:22
+ # @ Proyect: Aitea Building Lab
+ # @ Description:
+ # @ Copyright (c) 2024: Departamento de I+D. Aitea Tech
+ '''
+
+import json
+from loguru import logger
+import pandas as pd
+from datetime import datetime
+import dill
+
+
+def read_json_schedule_plan(path: str) -> dict:
+    """Read a json to have an execution plan, check if it has the basic configuration
+
+    Returns:
+        dict: Configuration plan execution
+    """
+    data = None
+    try:
+        with open(path, 'r') as archivo:
+            data = json.load(archivo)
+    except FileNotFoundError:
+        logger.error(f"Error: The '{path}' not exist.")
+    except json.JSONDecodeError:
+        logger.error(
+            f"Error: The file '{path}' is not well formed or has invalid JSON format.")
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+    else:
+        ok = check_configure(data)
+        if not ok:
+            data = None
+            logger.error(
+                "Error: Essential elements are missing in the pipeline configuration. Make sure it looks like this:  'pipe_name': { 'steeps': { 'analytic_file_name.Trasform_or_ModelCLass': {} }, 'datetime': { 'start': '', 'freq': ''}")
+    return data
+
+
+def check_configure(data_json: dict) -> bool:
+    """Check if the configuration is correct
+
+    Args:
+        data_json (dict): Configuration plan dictionary
+
+    Returns:
+        bool: Correct or incorrect
+    """
+    ok = True
+    for key, values in data_json.items():
+        if isinstance(values, dict):
+            keys = list(values.keys())
+            if 'steeps' not in keys or not 'freq_info' in keys or not 'training_query' in keys:
+                ok = False
+    return ok
+
+
+def lab_fit(data: pd.DataFrame, pipe_core: dict):
+    logger.info(f"Starting pipe fitting for {pipe_core}")
+    try:
+        pipe = pipe_core["pipe"]
+        query = pipe_core["training_query"]
+        name = pipe_core["name"]
+        try:
+            pipe.fit(data)
+        except Exception as err:
+            return err
+    except Exception as err:
+        return err
+    training_pipe = {"pipe_name": name, "pipe": pipe, "training_query": query,  "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+    return training_pipe
+
+def pipe_save(pipe_data:dict):
+    dill.settings['recurse'] = True
+    name = pipe_data.get("pipe_name")
+    path = "training_models/" + name + ".pkl"
+    with open(path, "wb") as f:
+        dill.dump(pipe_data, f)
+
+def load_pipe(path: str):
+    pipeline = None
+    with open(path, "rb") as f:
+        pipeline = dill.load(f)
+    return pipeline
