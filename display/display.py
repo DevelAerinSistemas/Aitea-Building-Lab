cc28810dd1797b482470daa21d8b57b11ac65d31
Jose Luis Blanco <joseluis.blanco@aitea.tech>
2025-06-16 17:39:41 +0200

    Se añaden algunas mejoras y se quita la ejecución aleatoria

--- display/display.py
+++ display/display.py
@@ -25,6 +25,7 @@ import streamlit as st
 import plotly.graph_objects as go
 from loguru import logger
 import random
+import time
 
 lib_dir = "lib"
 available_libraries = [f.split(".")[0] for f in os.listdir(lib_dir) if f.endswith(".so")]
@@ -50,10 +51,6 @@ def main():
         stop_date = st.date_input("Stop Date", datetime.date(2025, 1, 7))
         stop_time = st.time_input("Stop Time", datetime.time(23, 59))
     
-    
-    total_test = st.number_input("Total Tests", min_value=1, value=5, step=1)
-    temporal_windows = st.number_input("Temporal Window (hours)", min_value=1, value=24, step=1)
-    
     table_or_graph = st.radio("Display Results As:", ("Table", "Graph"), index=1)
 
     
@@ -70,99 +67,75 @@ def main():
                 start_date_str = start_datetime.isoformat(timespec='milliseconds') + 'Z'
                 stop_date_str = stop_datetime.isoformat(timespec='milliseconds') + 'Z'
                 logger.info(f"Start date: {start_date_str}, Stop date: {stop_date_str}")
-               
-                results = loader.testing(stop_date_str, start_date_str)
+                
+                time_init = time.time()
+                
+                results, result_matrix = loader.testing(stop_date_str, start_date_str)
+                
+                time_end = time.time()
+                
+                time_elapsed = time_end - time_init
+                st.markdown(f"<span style='font-size: 24px;'>Time elapsed: {time_elapsed:.2f} seconds for {library_name} for {len(results)} buckets.</span>", unsafe_allow_html=True)
+                
+                if table_or_graph == "Table":
+                    st.markdown("---")
+                    create_table(result_matrix)
+                else:
+                    data = []
+                    for bucket, prediction in results.items():
+                        if hasattr(prediction, 'columns'):
+                            for column in prediction.columns:
+                                data.append(go.Scatter(x=prediction.index, y=prediction[column], mode='lines', name=f"{bucket}-{column}"))
+                        else:
+                            logger.warning(f"Unexpected prediction format for bucket {bucket}: {type(prediction)}")
 
-               
-                data = []
-                for bucket, prediction in results.items():
-                    if hasattr(prediction, 'columns'):
-                        for column in prediction.columns:
-                            data.append(go.Scatter(x=prediction.index, y=prediction[column], mode='lines', name=f"{bucket}-{column}"))
-                    else:
-                        logger.warning(f"Unexpected prediction format for bucket {bucket}: {type(prediction)}")
 
-               
-                fig = go.Figure(data=data)
-                fig.update_layout(title="Testing Results", xaxis_title="Time", yaxis_title="Value")
+                    fig = go.Figure(data=data)
+                    fig.update_layout(title="Testing Results", xaxis_title="Time", yaxis_title="Value")
 
-              
-                st.plotly_chart(fig)
+
+                    st.plotly_chart(fig)
 
             except Exception as e:
                 logger.error(f"Error during execution: {e}")
                 st.error(f"Error during execution: {e}")
         else:
             st.warning("Please select a library and dates.")
-    
-    if st.button("Execute Random Testing"):
-        if library_name:
-            try:
-                # Load the selected library
-                loader = SOLibraryLoader(library_name)
-                
 
-                year = 2025
-          
-                for i in range(total_test):
-                    month = random.randint(1, 5)
-                    if month == 2:
-                        day = random.randint(1, 28)  
-                    else:
-                        day = random.randint(1, 31) 
-                    start_date = datetime.date(year, month, day)
-        
-                    random_hour = random.randint(0, 23)
-                    random_time = datetime.time(random_hour, 0, 0)
-
-                    start_datetime = datetime.datetime.combine(start_date, random_time)
-                    
-                    test_stop_datetime = start_datetime + datetime.timedelta(hours=temporal_windows)
-                    
-                    logger.info(f"Random Test {i+1}: Start - {start_datetime}, Stop - {test_stop_datetime}")
-                    # Format dates to strings
-                    test_start_str = start_datetime.isoformat(timespec='milliseconds') + 'Z'
-                    test_stop_str = test_stop_datetime.isoformat(timespec='milliseconds') + 'Z'
-
-                    # Execute testing
-                    results = loader.testing(test_stop_str, test_start_str)
-                    
-                    data = []
-                    if table_or_graph == "Table":
-                        st.subheader(f"Random Test {i+1} Results")
-                        st.markdown(f"**Start Time:** {start_datetime} - **Stop Time:** {test_stop_datetime}")
-                        st.markdown("---")
-                        
-                        # Create a table for each bucket
-                        for bucket, prediction in results.items():
-                            st.markdown(f"### Bucket: {bucket}")
-                            if hasattr(prediction, 'columns'):
-                                st.dataframe(prediction)
-                            else:
-                                logger.warning(f"Unexpected prediction format for bucket {bucket}: {type(prediction)}")
-                    else:
-                        for bucket, prediction in results.items():
-                            if hasattr(prediction, 'columns'):
-                                for column in prediction.columns:
-                                    data.append(go.Scatter(x=prediction.index, y=prediction[column], mode='lines', name=f"{bucket}-{column}"))
-                            else:
-                                logger.warning(f"Unexpected prediction format for bucket {bucket}: {type(prediction)}")
-
-                        # Create figure
-                        fig = go.Figure(data=data)
-                        fig.update_layout(title=f"Random Test {i+1}- {start_datetime}, Stop - {test_stop_datetime}: Results", xaxis_title="Time", yaxis_title="Value")
-
-                        # Display graph
-                        st.plotly_chart(fig)
-                    
-
-            except Exception as e:
-                logger.error(f"Error during random execution: {e}")
-                st.error(f"Error during random execution: {e}")
-        else:
-            st.warning("Please select a library.")
     
     
-
+def create_table(data_dictionary):
+    st.markdown("---")
+                    
+    # Create a table for each bucket
+    for bucket, prediction in data_dictionary.items():
+        for name, matrix in prediction[-1].items():
+            st.markdown(f"### {bucket} - {name}")
+            if hasattr(matrix, 'columns'):
+                st.dataframe(matrix)
+                st.markdown(f"### {bucket} - {name}. NaN table")
+                st.dataframe(matrix[matrix.isna().any(axis=1)])
+                st.markdown(f"### {bucket} - {name}. dtypes")
+                st.dataframe(matrix.dtypes.to_frame(name='Dtype'))
+                st.markdown(f"### {bucket} - {name}. Description")
+                st.dataframe(matrix.describe())
+            else:
+                logger.warning(f"Unexpected prediction format for bucket {bucket}: {type(matrix)}")
+    
+def create_graph(data_dictionary):
+    st.markdown("---")
+    
+    # Create a graph for each bucket
+    for bucket, prediction in data_dictionary.items():
+        for name, matrix in prediction[-1].items():
+            st.markdown(f"### {bucket} - {name}")
+            if hasattr(matrix, 'columns'):
+                fig = go.Figure()
+                for column in matrix.columns:
+                    fig.add_trace(go.Scatter(x=matrix.index, y=matrix[column], mode='lines', name=f"{bucket}-{name}-{column}"))
+                fig.update_layout(title=f"Graph for {bucket} - {name}", xaxis_title="Time", yaxis_title="Value")
+                st.plotly_chart(fig)
+            else:
+                logger.warning(f"Unexpected prediction format for bucket {bucket}: {type(matrix)}")
 if __name__ == "__main__":
     main()
