71d3e7ad8a7b77ca85562376bd8a9174c309bd79
Jose Luis Blanco <joseluis.blanco@aitea.tech>
2025-07-14 13:59:57 +0200

    Cabeceras

--- exceptions/fit_exception.py
+++ exceptions/fit_exception.py
@@ -1,3 +1,18 @@
+'''
+ # @ Project: AItea-Building-Lab
+ # @ Author: Aerin S.L.
+ # @ Create Time: 2025-05-29 10:35:08
+ # @ Description:
+ # @ Version: 1.0.0
+ # @ -------:
+ # @ Modified by: Aerin S.L.
+ # @ Modified time: 2025-05-29 10:35:12
+ # @ License: This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
+; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
 
 class InsufficientDataError(Exception):
     def __init__(self, msg="There is not enough data"):
--- metaclass/templates.py
+++ metaclass/templates.py
@@ -1,12 +1,19 @@
 '''
- # @ Author: Jose Luis Blanco Garcia-Moreno <joseluis.blanco@aitea.tech>
- # @ Create Time: 2024-09-09 11:15:44
- # @ Modified by: Jose LUis Blanco Garcia-Moreno
- # @ Modified time: 2024-09-09 11:15:48
- # @ Proyect: Aitea Building Lab
- # @ Description: Metaclasses and templates. 
- # @ Copyright (c) 2024: Departamento de I+D. Aitea Tech
- '''
+ # @ Project: AItea-Brain-Lite
+ # @ Author: Aerin S.L.
+ # @ Create Time: 2024-11-09 11:15:44
+ # @ Description:
+ # @ Version: 1.0.0
+ # @ -------:
+ # @ Modified by: Aerin S.L.
+ # @ Modified time: 2024-11-09 11:15:44
+ # @ License: This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
+; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+
 
 from typing import Any
 import numpy as np
